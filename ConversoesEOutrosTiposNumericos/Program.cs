cc18013b842464b25c4ce5afd348959af700c1dc
Henrique Mapa <30707089+HenriqueCamilo@users.noreply.github.com>
2019-04-21 16:38:48 -0300

    Add files via upload

---
+++ ConversoesEOutrosTiposNumericos/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _4_ConversoesEOutrosTiposNumericos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 4");
+
+            double salario = 1200.50;
+
+            int salarioInteiro; // variavel de 32 bits
+
+            salarioInteiro = (int)salario;
+
+            Console.WriteLine(salarioInteiro);
+
+            //long idade;
+            //idade = 13000000000000000; Quantidade de valor muito grande, onde o int n cabe 64 bits
+
+            //short quantidadeProdutos = 150;
+            //Quantidade muito pequena! 16 bits
+
+            float altura = 1.80f;
+            Console.WriteLine(altura);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TestaPontoFlutuante/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TestaPontoFlutuante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double salario = 1250.70;
+
+            Console.WriteLine($"O meu salario é {salario}");
+
+
+            double divisao = 5.0 / 2;
+
+            Console.WriteLine(divisao);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
