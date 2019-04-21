4b3c74884df8be42da65a3a2545275c21da5c1f8
Henrique Mapa <30707089+HenriqueCamilo@users.noreply.github.com>
2019-04-21 16:54:49 -0300

    Add files via upload

---
+++ P13-ForEncadeado/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace P13_ForEncadeado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Projeto 13 - For encadeado!");
+
+            // *
+            // **
+            // ***
+            // ****
+
+            for(int contadorLinha= 0; contadorLinha<10; contadorLinha++)
+            {
+                for(int contadorColuna = 0; contadorColuna <contadorLinha; contadorColuna++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ TransformaWhileEmFor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TransformaWhileEmFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Transforma While em For!");
+
+            int contador = 1;
+            for(; contador<= 10; contador++)
+            {
+                Console.WriteLine(contador);
+
+            }
+        }
+    }
+}
